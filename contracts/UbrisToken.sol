7c5651c4f200e34726e7c2e6d200e90a67a332b9
SY-HHK <louis.ricour@gmail.com>
2020-12-18 14:28:30 +0100

    stacking

--- contracts/UbrisToken.sol
+++ contracts/UbrisToken.sol
@@ -15,4 +15,60 @@ contract UbrisToken is ERC20 {
         require(balanceOf(msg.sender) < 10, "You have to much tokens to use this faucet");
         this.transfer(msg.sender, 1);
     }
+
+     //////////////////////////////////
+    //             POOL             //
+   //////////////////////////////////
+
+    struct StackedIn {
+        uint date;
+        uint amount;
+    }
+
+    struct Stacker {
+        StackedIn[] stack;
+        uint lastHarvestDate;
+    }
+
+    mapping (address => Stacker) stackedAmount;
+
+    //add stack in pool
+    function stackIn(uint _amount) public {
+        require(_amount > 0);
+        require(balanceOf(msg.sender) >= _amount);
+        if (transferFrom(msg.sender, address(this), _amount)) {
+            StackedIn memory newStack = StackedIn(block.timestamp, _amount);
+            stackedAmount[msg.sender].stack.push(newStack);
+        }
+    }
+
+    //retire stack from pool
+    function stackOut() public {
+        Stacker memory stacker = stackedAmount[msg.sender];
+        for (uint i = 0; i < stacker.stack.length; i+=1) {
+            transfer(msg.sender, stacker.stack[i].amount);
+        }
+        delete stacker.stack;
+    }
+
+    //calcul harvest
+    function getHarvestable() view public returns(uint) {
+        uint harvestable = 0;
+        Stacker memory stacker = stackedAmount[msg.sender];
+        for (uint i = 0; i < stacker.stack.length; i+=1) {
+            if (stacker.lastHarvestDate > stacker.stack[i].date) {
+                harvestable += (stacker.stack[i].amount / 100) * (stacker.lastHarvestDate / 3600 minutes);
+            }
+            else {
+                harvestable += (stacker.stack[i].amount / 100) * (stacker.stack[i].date / 3600 minutes);
+            }
+        }
+        return harvestable;
+    }
+
+    //harvest
+    function harvest() public {
+        transferFrom(address(this), msg.sender, getHarvestable());
+        stackedAmount[msg.sender].lastHarvestDate = block.timestamp;
+    }
 }
